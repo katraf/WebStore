001ed68647f1174e9f71f7a1231af6f04f2e76d1
Grishin Ivan <GrishinI@terralink-global.com>
2021-04-20 19:07:14 +0300

    Employees controller added

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петя", Patronymic = "Максимович", Age = 81 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Иван", Patronymic = "Петрович", Age = 58 },
+        };
+
+        public IActionResult Index()
+        {
+            return View(_Employees);
+        }
+    }
+}
