7b1972b577b598759e9deabe274e187bdc324fd4
Grishin Ivan <GrishinI@terralink-global.com>
2021-04-21 01:28:42 +0300

    fix addresses to absolute

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -4,21 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Models;
+using WebStore.Data;
 
 namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _Employees = new()
+        private readonly List<Employee> _Employees;
+
+        public EmployeesController()
         {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Петя", Patronymic = "Максимович", Age = 81 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Иван", Patronymic = "Петрович", Age = 58 },
-        };
+            _Employees = TestData.Employees;
+        }
 
         public IActionResult Index()
         {
             return View(_Employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null)
+                return NotFound();
+
+            return View(employee);
+
         }
     }
 }
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    internal static class TestData
+    {
+        public static List<Employee> Employees { get;  } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петя", Patronymic = "Максимович", Age = 81 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Иван", Patronymic = "Петрович", Age = 58 },
+        };
+    }
+}
