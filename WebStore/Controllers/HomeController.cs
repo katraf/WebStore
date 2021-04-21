5b106687d1e27c09712e86c73d40c321fe4b0e61
Grishin Ivan <GrishinI@terralink-global.com>
2021-04-21 21:37:28 +0300

    add Home views and controller actions

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -12,5 +12,16 @@ namespace WebStore.Controllers
         }
 
         public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
+
+        public IActionResult Not_Found() => View();
+        public IActionResult Blog() => View();
+        public IActionResult Blog_single() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult Contact_us() => View();
+        public IActionResult Login() => View();
+        public IActionResult Product_details() => View();
+        public IActionResult Shop() => View();
+
     }
 }
